5ee492834b0efb8446a8d98de88c4588b3f2b97f
Ansh_Jamwal <139267839+ANSH-1n@users.noreply.github.com>
2025-01-11 16:30:01 +0530

    Updated server.js with new API route /api/get_user_details

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get('/api/get', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.get('/api/get_user_details', (req, res) => {
+    res.send({
+        user : {
+            name : "Ansh",
+            age : 20,
+            contact : 172288282
+        }
+    });
+  });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
